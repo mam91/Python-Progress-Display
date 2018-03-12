78017181876f469e518180e34d1004f7546902c0
mam91 <mmiller3.ar@gmail.com>
2018-03-12 15:35:03 -0500

    Updates
    Modified name and print line to print on same line.

--- pyprogress/progress.py
+++ pyprogress/progress.py
@@ -1,13 +1,20 @@
 class progress(object):
 	def __init__(self, total):
 		self.percent = -1
+		self.backspaceLen = 0
 		self.total = total
  
 	def updatePercent(self, current):
 		newPercent = int(current / self.total * 100)
 		if newPercent > self.percent:
+			self.backspaceLen = 0 if self.percent < 0 else len(str(self.percent)) + 1
 			self.percent = newPercent
 			self.printPercent()
-			
+
 	def printPercent(self):
-		print(" " + str(self.percent) + "%\r", end="", flush=True)
+		backspaceStr = ""
+		for _ in range(self.backspaceLen): backspaceStr += "\b"
+		print(backspaceStr + str(self.percent) + "%", end="", flush=True)
+
+	def close(self):
+		print("", end="\n", flush=True)
